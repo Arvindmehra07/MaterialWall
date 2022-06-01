7f552474a5cf7bf2f62252ecce1e328308e39818
Arvind <31096704+Arvindmehra07@users.noreply.github.com>
2022-06-01 11:22:48 +0530

    Update Constants.kt
    removed api key

--- app/src/main/java/com/example/materialwall/common/Constants.kt
+++ app/src/main/java/com/example/materialwall/common/Constants.kt
@@ -3,6 +3,6 @@ package com.example.materialwall.common
 object Constants {
 
     const val BASE_URL = "https://pixabay.com/"
-    const val API_KEY = "24758393-3f31f16c5c9667f1933baeeef"
+    const val API_KEY = "nice_try"
     const val IMAGE_ID = "imageId"
 }
